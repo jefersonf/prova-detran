4610ad7240017c2e3d4a7fb110be721510157e7c
jeferson <jefersonnpn@gmail.com>
2024-03-24 20:51:06 -0300

    style: removig unnecessary comments

--- main.go
+++ main.go
@@ -4,21 +4,6 @@ import (
 	"github.com/jefersonf/prova-detran/cmd"
 )
 
-// var questionSetFile = "./internal/data/questions.json"
-
 func main() {
-
 	cmd.Execute()
-
-	// jsonFile, err := os.Open(questionSetFile)
-	// checkErr(err)
-	// defer jsonFile.Close()
-
-	// qset, err := parser.ParseQuestionSet(jsonFile)
-	// if err != nil {
-	// 	fmt.Println(err)
-	// }
-
-	// fmt.Println(qset)
-
 }

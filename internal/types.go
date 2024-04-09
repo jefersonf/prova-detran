7649b3c2ac17efac02587adb938519419d218580
Jeferson Ferreira <jefersonnpn@gmail.com>
2024-04-08 22:48:16 -0300

    add comments

--- internal/types.go
+++ internal/types.go
@@ -2,10 +2,12 @@ package internal
 
 import "fmt"
 
+// QuestionSet ...
 type QuestionSet struct {
 	Questions []Question `json:"questoes"`
 }
 
+// Question ...
 type Question struct {
 	Topic        string        `json:"tema"`
 	Statement    string        `json:"enunciado"`
@@ -13,16 +15,19 @@ type Question struct {
 	RightAnswer  string        `json:"correta"`
 }
 
+/ Alternative represents a prompt question option.
 type Alternative struct {
 	Label     string `json:"opcao"`
 	Statement string `json:"alternativa"`
 }
 
+// PromptQuestion represents a prompt question statement.
 type PromptQuestion struct {
 	Text       string
 	QuestionID string
 }
 
+// FormattedAlternatives returns a list of pre-formatted question statements.
 func (q *Question) FormattedAlternatives(questionID int) []PromptQuestion {
 	formatted := make([]PromptQuestion, len(q.Alternatives))
 	for i, a := range q.Alternatives {
